b68ee67df27944684e22e58f272fd0d3f263dba1
ii0703 <42719196+ii0703@users.noreply.github.com>
2024-10-25 04:03:24 +0000

    inicial

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from random import randint
 
 import pandas as pd
 
-periodos: List["Periodo"] = PeriodoUtil.generarPeriodosPorNumeroDias(
+periodos: List['Periodo'] = PeriodoUtil.generarPeriodosPorNumeroDias(
     date(2024, month=1, day=1), cantidad_periodos=7, cantidad_dias=2
 )
 
-productos: List["Producto"] = []
-productos.append(Producto(codigo="A01", nombre="Sillas de Camping"))
-productos.append(Producto(codigo="A02", nombre="Sillas de Camping con reposamanos"))
-productos.append(Producto(codigo="C01", nombre="Tiendas de campañas de 2 personas"))
-productos.append(Producto(codigo="C02", nombre="Tiendas de campañas de 4 personas"))
-productos.append(Producto(codigo="C03", nombre="Tiendas de campañas de 6 personas"))
+productos: List['Producto'] = []
+productos.append(Producto(codigo='A01', nombre='Sillas de Camping'))
+productos.append(Producto(codigo='A02', nombre='Sillas de Camping con reposamanos'))
+productos.append(Producto(codigo='C01', nombre='Tiendas de campañas de 2 personas'))
+productos.append(Producto(codigo='C02', nombre='Tiendas de campañas de 4 personas'))
+productos.append(Producto(codigo='C03', nombre='Tiendas de campañas de 6 personas'))
 
 print(periodos)
 print(productos)
